817966212ae6dc06c5cd10e4374e8ec0b441ae46
ajt <48979824+ankitjt@users.noreply.github.com>
2022-07-18 14:13:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,48 @@ let createTaskScreen = document.querySelector(".createTaskScreen"),
     createTaskNavLink = document.querySelector(".createTaskNavLink"),
     closeCreateTaskScreen = document.querySelector(".closeCreateTaskScreen"),
     downArrows = document.querySelectorAll(".downArrow"),
-    upArrow = document.querySelectorAll(".upArrow"),
+    upArrows = document.querySelectorAll(".upArrow"),
     prioritySection = document.querySelector(".prioritySection"),
-    taskStats = document.querySelector(".taskStats")
+    taskStats = document.querySelectorAll(".taskStats")
 
 createTaskNavLink.onclick = () => {
-    console.log('20');
     createTaskScreen.classList.add("block")
     createTaskScreen.classList.remove("hidden")
+    createTaskScreen.style.top = 0
 }
 
 closeCreateTaskScreen.onclick = () => {
     createTaskScreen.classList.add("hidden")
 }
 
-for (let thisDrop of downArrows) {
-    console.log(thisDrop)
-    thisDrop.onclick = () => {
-        // let selectedTab = thisDrop.parentElement.parentElement.parentElement.childNodes[7]
-        // selectedTab.classList.toggle("hidden")
-        // console.log(selectedTab)
-        // for (let thisSelectedTab of selectedTab) {
-        // }
-        // let a = selectedTab.querySelector(".taskStats")
-        // a.classList.toggle("hidden")
+// for (let i = 0; i < downArrows.length; i++) {
+//     downArrows[i].onclick = () => {
+//         console.log(downArrows[i].parentElement.parentElement.parentElement.innerHTML)
+//     }
+// }
+
+for (let thisDownArrow of downArrows) {
+    thisDownArrow.onclick = () => {
+        let selectedBox = thisDownArrow.parentElement.parentElement.parentElement.childNodes[7]
+        let upArrowBtn = thisDownArrow.parentElement.childNodes[3]
+        upArrowBtn.classList.remove("hidden")
+        thisDownArrow.classList.add("hidden")
+        selectedBox.classList.remove("hidden")
+    }
+}
+
+for (let thisUpArrow of upArrows) {
+    thisUpArrow.onclick = () => {
+        let selectedBox = thisUpArrow.parentElement.parentElement.parentElement.childNodes[7]
+        let upArrowBtn = thisUpArrow.parentElement.childNodes[3]
+        let bringDownArrow = thisUpArrow.parentElement.childNodes[1]
+        upArrowBtn.classList.add("hidden")
+        bringDownArrow.classList.remove("hidden")
+        selectedBox.classList.add("hidden")
     }
 }
 
 
-upArrow.onclick = () => {
-    upArrow.classList.add("hidden")
-    taskStats.classList.add("hidden")
-    downArrow.classList.remove("hidden")
-}
+
 
 
